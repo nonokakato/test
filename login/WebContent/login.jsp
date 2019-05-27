e21a2e845631db4ddb6777508ca765e8e8f52082
nonokakato <nonofraise75@gmail.com>
2019-05-27 21:33:50 +0900

    0527

--- login/WebContent/login.jsp
+++ login/WebContent/login.jsp
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset=UTF-8">
+  <meta charset="UTF-8">
   <title>ログイン画面</title>
 </head>
 
---
+++ login2/src/com/internousdev/login2/util/DBConnector.java
@@ -0,0 +1,13 @@
+package com.internousdev.login2.util;
+
+public class DBConnector {
+
+	private static String driverName="com.mysql.jdbc.Driver";
+	private static String url="jdbc:mysql://localhost/logindb_nkato";
+
+	private static String user="root";
+	private static String password="mysql";
+
+
+
+}
