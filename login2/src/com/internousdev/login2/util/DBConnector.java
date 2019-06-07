f29c255f295043e8f6a0a65d25c1f3edaf088eb5
nonokakato <nonofraise75@gmail.com>
2019-06-07 21:42:44 +0900

    0607

--- login2/src/com/internousdev/login2/util/DBConnector.java
+++ login2/src/com/internousdev/login2/util/DBConnector.java
@@ -1,4 +1,7 @@
 package com.internousdev.login2.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnector {
 
@@ -8,6 +11,22 @@ public class DBConnector {
 	private static String user="root";
 	private static String password="mysql";
 
+    public Connection getConnection(){
+    	Connection con=null;
 
+    	try{
+    		Class.forName(driverName);
+    		con=(Connection)DriverManager.getConnection(url,user,password);
+
+    	}catch(ClassNotFoundException e){
+    		e.printStackTrace();
+
+    	}catch(SQLException e){
+    		e.printStackTrace();
+
+    	}
+
+    	return con;
+    }
 
 }
